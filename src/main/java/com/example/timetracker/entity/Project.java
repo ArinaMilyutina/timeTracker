780d31de8e552d7798302ae288bd48c1e155e9c2
Arina <arina.milyutina.0005@gmail.com>
2024-10-24 19:11:58 +0300

    created entity Project

---
+++ src/main/java/com/example/timetracker/entity/Project.java
@@ -0,0 +1,33 @@
+package com.example.timetracker.entity;
+
+import com.example.timetracker.entity.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "projects")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    @ManyToOne
+    @JoinColumn(name = "admin_id")
+    private User admin;
+    @OneToMany(mappedBy = "project")
+    private List<Task> tasks;
+
+}
